2dc13b709341cb52946fe2ad085fc386363916a0
Venkata Narayana <kknarayana408@gmail.com>
2025-12-24 16:41:12 +0530

    assigment1

---
+++ Personal_Details_Using_Primitive_Data_Types.java
@@ -0,0 +1,27 @@
+package assignments;
+
+public class Personal_Details_Using_Primitive_Data_Types {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte age = 25;
+		System.out.print("Age : ");
+		System.out.println(age);
+		
+		float height = 165.6f;
+		System.out.print("Height : ");
+		System.out.println(height);
+		
+		long Mobile_number = 7095136278L;
+		System.out.print("Mobile Number : ");
+		System.out.println(Mobile_number);
+		
+		
+		boolean student = true;
+		System.out.print("Is Student : ");
+		System.out.println(student);
+
+
+	}
+
+}
