44fbffd0025f9926f31962402c0c04b30e340344
Venkata Narayana <kknarayana408@gmail.com>
2025-12-24 17:00:27 +0530

    Assignment1

---
+++ Display_All_Primitive_Data_Types.java
@@ -0,0 +1,43 @@
+package assignments;
+
+public class Display_All_Primitive_Data_Types {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte byte_value =10;
+		System.out.print("Byte value = ");
+		System.out.println(byte_value);
+		
+		short short_value = 100;
+		System.out.print("Short Value = ");
+		System.out.println(short_value);
+		
+		int int_value = 5000;
+		System.out.print("Int value = ");
+		System.out.println(int_value);
+		
+		long long_value = 9000000000L;
+		System.out.print("Long value = ");
+		System.out.println(long_value);
+		
+		float float_value = 5.5F;
+		System.out.print("Float Value = ");
+		System.out.println(float_value);
+		
+		double double_value = 99.987654321;
+		System.out.print("Double value = ");
+		System.out.println(double_value);
+		
+		char char_value = 'A';
+		System.out.print("Char value = ");
+		System.out.println(char_value);
+		
+		boolean boolean_value = true;
+		System.out.print("Boolean value = ");
+		System.out.println(boolean_value);
+
+
+	}
+
+}
